e687adb32e3eaef8cfd44175aeed9672f5e22cbf
Jared Hubbard <hubbard.abstract@gmail.com>
2020-06-22 17:16:37 -0700

    Update AbstractionClass.py

--- AbstractionClass.py
+++ AbstractionClass.py
@@ -2,19 +2,21 @@
 
 from abc import ABC, abstractmethod
 class phone(ABC):
-    def total(self, amount):
-        print("You new iPhone purchase comes out to: ",amount)
     @abstractmethod
-    def payment(self, amount):
+    def total(self, amount):
+        print('You new iPhone purchase comes out to: {}'.format(amount))
+    def payment(amount):
         pass
 
-    class FinancedPayments(ABC):
-        @staticmethod
-        def payment(amount):
-            print('Or you can finance your new iPhone for 3 years for {} a month '.format(amount))
+class FinancedPayments(ABC):
+    @staticmethod
+    def payment(amount):
+        print('Or you can finance your new iPhone for 3 years for {} a month '.format(amount))
+    def total(self, amount):
+        print('Giving you a grand total of... {}'.format(amount))
 
-    obj = FinancedPayments()
-    obj.total("$1200")
-    obj.payment("$54")
+obj = FinancedPayments()
+obj.total("$1200")
+obj.payment("$54")
 
 
